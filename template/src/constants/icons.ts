71d786ca41cc1ff207621813c671020f2756595d
Leon Arantes <64018468+LeonArantes@users.noreply.github.com>
2021-06-16 22:08:23 -0300

    chore: config project

---
+++ template/src/constants/icons.ts
@@ -0,0 +1,8 @@
+/**
+ *  Constants Icons 
+ *  Example: 
+ *    1- Import icon from assets  
+ *        import IconMenu from '../assets/icons/menu.svg'
+ *    2- Export icon from global App
+ *       export const ICON_MENU = IconMenu 
+ */
